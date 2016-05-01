094c5a779c317aa1aff87a4295974889935bd3c8
lindsaysteinbach <lsteinbach@google.com>
2016-05-01 18:26:26 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray() {
+  var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return array;
+}
+
+function addElementToArray (array) {
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  array[1] = "Carter";
+  return array;
+}
+
+function removeElementFromArray(array) {
+  array.splice(2,2,"Roast Chicken");
+  return array;
+}
+
+function iterateArray(numbers) {
+  var newNums = [];
+  numbers.forEach(function(number) {
+    newNums.push(number+5);
+  });
+  return newNums;
+}
